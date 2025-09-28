542e457a4f9faeae009eb2e4ff3909b3a8b513ae
MRifkyAlmadani <2510817310018@mhs.ulm.ac.id>
2025-09-28 11:50:51 +0800

    Add files via upload

---
+++ PRAK002-2510817310018-M_RIFKY_ALMADANI.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Selamat Pagi, M. Rifky Almadani\n");
+    printf("Selamat Siang, M. Rifky Almadani\n");
+    printf("Selamat Malam, M. Rifky Almadani\n");
+    return 0;
+}
