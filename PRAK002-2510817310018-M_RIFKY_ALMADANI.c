d7ed3c66ad80cf76ec7714f2799dcd92faad1a51
MRifkyAlmadani <2510817310018@mhs.ulm.ac.id>
2025-12-05 12:46:15 +0700

    Delete PRAK002-2510817310018-M_RIFKY_ALMADANI.c

--- PRAK002-2510817310018-M_RIFKY_ALMADANI.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Selamat Pagi, M. Rifky Almadani\n");
-    printf("Selamat Siang, M. Rifky Almadani\n");
-    printf("Selamat Malam, M. Rifky Almadani\n");
-    return 0;
-}
